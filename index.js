6d238eebdfc3b95892418a54d8eaed98cb262154
farah <farah@example.com>
2022-02-05 02:40:30 +0200

    aff

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ console.log("hello3");
 console.log("hello6");
 console.log("helllo5");
 
-onsole.log("hello4");
+console.log("hello4");
 console.log("hello3");
 console.log("helllo5");
 console.log("hello3");
@@ -34,3 +34,7 @@ console.log("hello2");
 console.log("hello4");
 
 
+
+
+console.log("hello14");
+console.log("hello15");
